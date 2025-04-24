0bdc2f1076ba2ccceed9aa1ef2ed1f4b1f9a2907
Hamza Ateek <hamzaateek@gmail.com>
2025-04-24 23:34:00 +0300

    add event and some styles first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Calender from "./components/claender/Calender";
+function App() {
+  return (
+    <div className="App">
+      <Calender />
+    </div>
+  );
+}
+
+export default App;
